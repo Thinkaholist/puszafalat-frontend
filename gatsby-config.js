e2331faaf20dd1bd7c8594c0f54bb9960cc1f470
Richárd Németh <nemethricsi@gmail.com>
2021-12-06 09:24:47 +0100

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,28 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Puszafalat",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'Puszafalat',
   },
   plugins: [
     {
-      resolve: "gatsby-source-sanity",
+      resolve: 'gatsby-source-sanity',
       options: {
-        projectId: "6h8tota2",
-        dataset: "Production",
+        projectId: '6h8tota2',
+        dataset: 'production',
       },
     },
-    "gatsby-plugin-styled-components",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
     },
   ],
 };
