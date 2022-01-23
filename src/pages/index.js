f628b410c69fbe5db60d272b7c6c0920ea819b0a
Richárd Németh <nemethricsi@gmail.com>
2022-01-23 19:19:30 +0100

    add BandCamp section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-plugin-sanity-image';
 import Layout from '../components/Layout';
 import localize from '../components/localize';
-import { ContainerStyles } from '../styles/ContainerStyles';
-import BandCampParser from '../components/BandCampParser';
 import HeroSection from '../components/HeroSection';
+import BandCampSection from '../components/BandCampSection';
 
 const IndexPage = ({ data, location }) => {
   const { albumEmbedCode } = data.homePage;
+
   return (
     <>
       <Layout header={data.header} footer={data.footer} location={location}>
@@ -17,33 +16,10 @@ const IndexPage = ({ data, location }) => {
           headerText={data.homePage.headerText}
           subHeaderText={data.homePage.subHeaderText}
         />
-        {/* <ContainerStyles>
-          <div style={{ display: 'grid', placeContent: 'center' }}>
-            <div style={{ width: '70%', margin: '0 auto' }}>
-              <Img
-                {...data.homePage.heroImage.image}
-                alt={data.homePage.heroImage.altText}
-                width={800}
-                style={{ width: '100%' }}
-              />
-            </div>
-            <h1
-              style={{
-                fontSize: 30,
-                margin: '32px 0',
-                fontWeight: 700,
-                textAlign: 'center',
-              }}
-            >
-              {data.homePage.headerText}
-            </h1>
-            <p>{data.homePage.subHeaderText}</p>
-            <p>{data.homePage.bandCampText}</p>
-          </div>
-          <div style={{ margin: '16px auto', maxWidth: 450 }}>
-            <BandCampParser albumCode={albumEmbedCode} />
-          </div>
-        </ContainerStyles> */}
+        <BandCampSection
+          bandCampText={data.homePage.bandCampText}
+          albumEmbedCode={albumEmbedCode}
+        />
       </Layout>
     </>
   );
