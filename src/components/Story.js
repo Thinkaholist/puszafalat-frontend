e05f4e7e4b2ad4c4f117577ca667d36d811d1e75
Richárd Németh <nemethricsi@gmail.com>
2022-02-13 14:17:31 +0100

    refactor story title and origin

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,13 +3,39 @@ import styled from 'styled-components';
 import Img from 'gatsby-plugin-sanity-image';
 import { lineBreaker } from '../utils/lineBreaker';
 import { TextContainerStyles } from '../styles/TextContainerStyles';
+import { QUERIES } from '../constants';
 
 const StoryTitle = styled.h1`
+  font-weight: 800;
+  font-size: ${18 / 16}rem;
+  text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: ${36 / 16}rem;
+    text-align: left;
+    margin-top: 50px;
+  }
+`;
+
+const Origin = styled.h2`
+  font-weight: 300;
+  font-size: ${15 / 16}rem;
   margin-bottom: 19px;
+  text-align: center;
+
+  @media ${QUERIES.tabletAndUp} {
+    text-align: left;
+    font-size: ${20 / 16}rem;
+    margin-bottom: 34px;
+  }
 `;
 
 const StoryBody = styled.div`
   margin-bottom: 19px;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: ${20 / 16}rem;
+  }
 `;
 
 const StoryImageWrapper = styled.div`
@@ -22,6 +48,7 @@ const StoryImage = styled(Img)`
 
 export default function Story({
   title,
+  origin,
   story,
   illustration: { image, altText },
 }) {
@@ -31,6 +58,7 @@ export default function Story({
     <>
       <TextContainerStyles>
         <StoryTitle>{title}</StoryTitle>
+        <Origin>{origin}</Origin>
         <StoryBody>{lineBreakedStory}</StoryBody>
         <StoryImageWrapper>
           <StoryImage {...image} alt={altText} width={800} />
