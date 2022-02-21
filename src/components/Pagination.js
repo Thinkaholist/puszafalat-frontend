a30891521499a75fedbe367c6d465f5205015bff
Richard <35897699+nemethricsi@users.noreply.github.com>
2022-02-21 20:12:09 +0100

    fix pagination alignment (#29)

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,6 +12,7 @@ const PaginationWrapper = styled.div`
   @media ${QUERIES.tabletAndUp} {
     flex-direction: row;
     justify-content: center;
+    margin: 4rem 0;
   }
 `;
 
@@ -40,7 +41,9 @@ const LinkWrapper = styled(Link)`
   } */
 `;
 
-const PreviousWrapper = styled(LinkWrapper)`
+const PreviousWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
   flex: 4;
 `;
 
@@ -49,7 +52,10 @@ const ThreeDotWrapper = styled.div`
   gap: 7px;
   flex: 1;
 `;
-const NextWrapper = styled(LinkWrapper)`
+const NextWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
   @media ${QUERIES.tabletAndUp} {
     justify-content: flex-end;
     flex: 4;
@@ -77,46 +83,50 @@ export default function Pagination({
   return (
     <>
       <PaginationWrapper>
-        {previous.slug && (
-          <PreviousWrapper
-            to={previousLink}
-            type={previous.foodType.serialNumber}
-          >
-            <div style={{ transform: 'rotate(180deg)' }}>
-              <svg
-                width='40'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 75.99 42.87'
-              >
-                <g id='ILLU_01'>
-                  <path d='M55.56,0h-2A22.45,22.45,0,0,0,66.75,20.43H0v2H66.75A22.47,22.47,0,0,0,53.56,42.87h2A20.46,20.46,0,0,1,76,22.43v-2A20.45,20.45,0,0,1,55.56,0Z' />
-                </g>
-              </svg>
-            </div>
-            <p>{previous.title}</p>
-          </PreviousWrapper>
-        )}
+        <PreviousWrapper>
+          {previous.slug && (
+            <LinkWrapper
+              to={previousLink}
+              type={previous.foodType.serialNumber}
+            >
+              <div style={{ transform: 'rotate(180deg)' }}>
+                <svg
+                  width='40'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 75.99 42.87'
+                >
+                  <g id='ILLU_01'>
+                    <path d='M55.56,0h-2A22.45,22.45,0,0,0,66.75,20.43H0v2H66.75A22.47,22.47,0,0,0,53.56,42.87h2A20.46,20.46,0,0,1,76,22.43v-2A20.45,20.45,0,0,1,55.56,0Z' />
+                  </g>
+                </svg>
+              </div>
+              <p>{previous.title}</p>
+            </LinkWrapper>
+          )}
+        </PreviousWrapper>
         <ThreeDotWrapper>
           {Array.from(new Array(3)).map((_, idx) => (
             <Dot key={idx} idx={idx} />
           ))}
         </ThreeDotWrapper>
-        {next.slug && (
-          <NextWrapper to={nextLink} type={next.foodType.serialNumber}>
-            <p>{next.title}</p>
-            <div>
-              <svg
-                width='40'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 75.99 42.87'
-              >
-                <g id='ILLU_01'>
-                  <path d='M55.56,0h-2A22.45,22.45,0,0,0,66.75,20.43H0v2H66.75A22.47,22.47,0,0,0,53.56,42.87h2A20.46,20.46,0,0,1,76,22.43v-2A20.45,20.45,0,0,1,55.56,0Z' />
-                </g>
-              </svg>
-            </div>
-          </NextWrapper>
-        )}
+        <NextWrapper>
+          {next.slug && (
+            <LinkWrapper to={nextLink} type={next.foodType.serialNumber}>
+              <p>{next.title}</p>
+              <div>
+                <svg
+                  width='40'
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 75.99 42.87'
+                >
+                  <g id='ILLU_01'>
+                    <path d='M55.56,0h-2A22.45,22.45,0,0,0,66.75,20.43H0v2H66.75A22.47,22.47,0,0,0,53.56,42.87h2A20.46,20.46,0,0,1,76,22.43v-2A20.45,20.45,0,0,1,55.56,0Z' />
+                  </g>
+                </svg>
+              </div>
+            </LinkWrapper>
+          )}
+        </NextWrapper>
       </PaginationWrapper>
     </>
   );
