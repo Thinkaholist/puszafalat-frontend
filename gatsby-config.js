d46685203bc3c1362bb015323be6ad55a9204551
Richárd Németh <nemethricsi@gmail.com>
2022-02-23 08:48:02 +0100

    remove defaults from GTM config

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,9 +51,9 @@ module.exports = {
         defaultDataLayer: { platform: 'gatsby' },
 
         // Specify optional GTM environment details.
-        gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
-        gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
-        dataLayerName: 'YOUR_DATA_LAYER_NAME',
+        // gtmAuth: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING',
+        // gtmPreview: 'YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME',
+        // dataLayerName: 'YOUR_DATA_LAYER_NAME',
 
         // Name of the event that is triggered
         // on every Gatsby route change.
@@ -63,7 +63,7 @@ module.exports = {
         // Defaults to false
         enableWebVitalsTracking: true,
         // Defaults to https://www.googletagmanager.com
-        selfHostedOrigin: 'YOUR_SELF_HOSTED_ORIGIN',
+        // selfHostedOrigin: 'YOUR_SELF_HOSTED_ORIGIN',
       },
     },
   ],
