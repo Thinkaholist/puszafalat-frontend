797d7ccc057f31a1cd519dba13f31cb2e104612b
Richárd Németh <nemethricsi@gmail.com>
2022-02-24 01:07:52 +0100

    send data to slack

--- src/api/feedback.js
+++ src/api/feedback.js
@@ -1,4 +1,30 @@
-export default function handler(req, res) {
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
   console.log(req);
-  res.status(200).json(req.body);
+  const url =
+    'https://hooks.slack.com/services/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';
+
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  const data = {
+    type: req.body.type,
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  try {
+    const result = await fetch(url, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+      return res.json();
+    });
+    res.json(result);
+  } catch (err) {
+    res.status(500).send(error);
+  }
 }
